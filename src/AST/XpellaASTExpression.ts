17763562b62a2c81a36925da5254723cb767c116
Guillaume Gravetot <ggravetot@gmail.com>
2019-03-29 16:34:50 +0100

    Added missing isInstance method.

--- src/AST/XpellaASTExpression.ts
+++ src/AST/XpellaASTExpression.ts
@@ -1,5 +1,6 @@
 import { XpellaASTStatement } from './XpellaASTStatement';
 import { XpellaASTAnnotation } from './XpellaASTAnnotation';
+import { XpellaASTNode } from './XpellaASTNode';
 
 export abstract class XpellaASTExpression extends XpellaASTStatement {
   public resolvedType: string;
@@ -7,5 +8,9 @@ export abstract class XpellaASTExpression extends XpellaASTStatement {
   constructor(annotations: XpellaASTAnnotation[], documentation: string, resolvedType: string) {
     super(annotations, documentation);
     this.resolvedType = resolvedType;
+  }
+
+  public static isInstance(other: XpellaASTNode): other is XpellaASTExpression {
+    return other instanceof XpellaASTExpression;
   }
 }
